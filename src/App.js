76c572933d4e7c65efad657d72439f927b2da851
Eduardo Ribeiro <eduardoribeiro17@gmail.com>
2020-04-20 11:15:24 -0300

    Desafio finalizado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'gostack-template-conceitos-reactjs',
+      owner: 'eduardoribeiro17',
+      url:
+        'https://github.com/eduardoribeiro17/gostack-template-conceitos-reactjs',
+      techs: ['React', 'ReactJS'],
+      likes: '2',
+    });
+    console.log(response.data);
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`).then((response) => {
+      if (response.status === 204) {
+        const newRepositories = repositories.filter((r) => r.id !== id);
+        setRepositories(newRepositories);
+      }
+    });
+
+    console.log(`Removido Repositório ID: `, id);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
